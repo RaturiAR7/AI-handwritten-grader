e56f8f43e230ff1c6d7b66f7009bcb0d17c986a1
Anshul <anshulraturi007@gmail.com>
2026-04-23 12:35:04 +0530

    fix upload file route to add examid

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -1,20 +1,29 @@
 import { NextRequest, NextResponse } from "next/server";
 import { extractTextFromPDF } from "@/lib/rag/pdf";
+import { processAndStoreExam } from "@/lib/rag/pipeline";
 
 export async function POST(req: NextRequest) {
   try {
     const formData = await req.formData();
     const file = formData.get("file") as File;
+    const examId = formData.get("examId") as string;
 
     if (!file) {
       return NextResponse.json({ error: "No file uploaded" }, { status: 400 });
+    }
+    if (!examId) {
+      return NextResponse.json(
+        { error: "No examId provided" },
+        { status: 400 },
+      );
     }
 
     const bytes = await file.arrayBuffer();
     const uint8Array = new Uint8Array(bytes);
 
     const extractedText = await extractTextFromPDF(uint8Array);
-    console.log(extractedText);
+
+    await processAndStoreExam(examId, extractedText.text);
 
     return NextResponse.json({
       success: true,
